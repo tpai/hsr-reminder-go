0f4edaed1e365aa064f1cc99baaec57bcf60712c
Tony Pai <tony77794@gmail.com>
2020-08-14 23:39:20 +0800

    Add retryAttempts prop and fix ts warning

--- cdk/index.ts
+++ cdk/index.ts
@@ -6,21 +6,21 @@ import cdk = require('@aws-cdk/core');
 require('dotenv').config();
 
 const {
-  ENDPOINT,
-  WEBHOOK,
-  FROM,
-  TO,
-  DATE,
-  TIMETABLE,
-  TICKETCOUNT,
-  CARRIAGECATEGORY,
-  ONLYSHOWDISCOUNT,
-  COLLEGESTUDENTS,
-  DEVICEID,
-  DEVICEIDHASH,
-  DEVICECATEGORY,
-  APPVERSION,
-  PARAMETERVERSION
+  ENDPOINT = "",
+  WEBHOOK = "",
+  FROM = "9",
+  TO = "2",
+  DATE = "2020-09-01",
+  TIMETABLE = "18:00",
+  TICKETCOUNT = "2",
+  CARRIAGECATEGORY = "0",
+  ONLYSHOWDISCOUNT = "0",
+  COLLEGESTUDENTS = "0",
+  DEVICEID = "",
+  DEVICEIDHASH = "",
+  DEVICECATEGORY = "I",
+  APPVERSION = "5.20",
+  PARAMETERVERSION = "20200527"
 } = process.env;
 
 export class LambdaCronStack extends cdk.Stack {
@@ -49,9 +49,10 @@ export class LambdaCronStack extends cdk.Stack {
     const lambdaFn = new lambda.Function(this, 'Singleton', {
       code: lambda.Code.fromAsset('./lambda-hsr_reminder.zip'),
       handler: 'lambda-hsr_reminder',
-      timeout: cdk.Duration.seconds(5),
       runtime: lambda.Runtime.GO_1_X,
-      environment: env
+      environment: env,
+      timeout: cdk.Duration.seconds(5),
+      retryAttempts: 0
     });
 
     const rule = new events.Rule(this, 'Rule', {
